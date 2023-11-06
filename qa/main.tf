48641830c05e668f753c21b120ba6e21f3bd2f1d
Vijay Natarajan <gentwo@yahoo.com>
2023-11-06 15:32:32 -0800

    define qa environment

---
+++ qa/main.tf
@@ -0,0 +1,13 @@
+module "qa" {
+    source= "../modules/blog"
+
+    environment = {
+        name = "qa"
+        network_prefix = "10.1"
+    }
+
+    autoscale {
+        min = 1
+        max = 1
+    }
+}
