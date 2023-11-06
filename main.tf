98fa3e3b1838c31116ec1d92ca92fe73eac21028
Vijay Natarajan <gentwo@yahoo.com>
2023-11-06 14:24:12 -0800

    autoscaling

--- main.tf
+++ main.tf
@@ -29,50 +29,22 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id = module.blog_vpc.public_subnets[0]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.2.0"
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 }
 
-# module "alb" {
-#  source  = "terraform-aws-modules/alb/aws"
-#  version = "9.1.0"
 
-#  name    = "blog-alb"
-#  vpc_id  = module.blog_vpc.vpc_id
-#  subnets = module.blog_vpc.public_subnets
-#  security_groups = [module.blog_sg.security_group_id]
-
-#  target_groups = {
-#    ex_instance = {
-#      name_prefix      = "blog-"
-#      backend_protocol = "HTTP"
-#      backend_port     = 80
-#      target_type      = "instance"
-#    }
-#  }
-#
-#  listeners = {
-#    ex_https = {
-#      port            = 80
-#      protocol        = "HTTP"
-#
-#      forward = {
-#        target_group_key = "ex_instance"
-#      }
-#    }
-#  }
-#
-#  tags = {
-#    Environment = "dev"
-#  }
-#}
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
